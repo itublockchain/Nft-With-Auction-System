2464521df21cd3cf452bafad7ef77cbdce89e06c
aalimsahin <73793382+aalimsahin@users.noreply.github.com>
2021-06-09 00:58:59 +0300

    Update Balance.js

--- src/components/reach/Balance.js
+++ src/components/reach/Balance.js
@@ -2,9 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../../Context";
 
 const Balance = () => {
-
     const [account, , balance] = useContext(Context);
-
     return (
         Object.keys(account).length !== 0
             ? (<span className=" ml-3 p-2" style={{
