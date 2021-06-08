63f15074e0e89513df28d3f02cd0be17bb9cb526
aalimsahin <73793382+aalimsahin@users.noreply.github.com>
2021-06-09 01:00:03 +0300

    Update FaucetButton.js

--- src/components/reach/FaucetButton.js
+++ src/components/reach/FaucetButton.js
@@ -1,21 +1,19 @@
 import React, { useState, useContext } from "react";
 import Nav from "react-bootstrap/Nav";
-
 import { Context } from "../../Context";
 import FaucetModal from "./FaucetModal";
 
-
 const FaucetButton = () => {
     const [account, , , setBalance] = useContext(Context);
-
     const [show, setShow] = useState(false);
-
     const handleOpen = () => setShow(true);
 
     return Object.keys(account).length !== 0 ?
         (
             <div>
-                <Nav.Link style={{color:"#fff"}}  onClick={handleOpen}>Faucet</Nav.Link>
+                <Nav.Link style={{color:"#fff"}}  onClick={handleOpen}>
+                    Faucet
+                </Nav.Link>
                 <FaucetModal setBalance={setBalance} account={account} show={show} setShow={setShow} unit="ALGO" />
             </div>
         ) : (<div />);
