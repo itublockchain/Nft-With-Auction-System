862796138074ca07bf3324dce4a4baadf27c293f
aalimsahin <73793382+aalimsahin@users.noreply.github.com>
2021-06-09 00:57:22 +0300

    Update BidderViews.js

--- src/components/participants/BidderViews.js
+++ src/components/participants/BidderViews.js
@@ -4,14 +4,14 @@ import Spinner from "react-bootstrap/Spinner";
 import Form from "react-bootstrap/Form";
 import { InformTimeout, SeeOutcome, IsAuctionOn, ShowBid } from "./PlayerViews";
 import Button from "react-bootstrap/Button";
-import { parseCurrency } from "@reach-sh/stdlib/ALGO";
-
+import {loadStdlib} from '@reach-sh/stdlib';
+const Reach = loadStdlib('ALGO');
 
 const BidderViews = ({appState, args, getBidReady, getBid, isAuctionOnReady, isAuctionOn}) => {
   switch (appState) {
     case "getBid":
       return getBidReady ? (
-        <GetBid nftViewAddress={args[0]} getBid={getBid} />
+        <GetBid getId ={args[0]} nftViewAddress={args[1]} getBid={getBid} />
       ) : (
         <Container style={{
           display: "grid",
@@ -22,27 +22,26 @@ const BidderViews = ({appState, args, getBidReady, getBid, isAuctionOnReady, isA
       }} className="mt-4">
           <h1>Waiting the Contract...</h1>
           <Button variant="info" disabled>
-        <Spinner
-          as="span"
-          animation="grow"
-          size="sm"
-          role="status"
-          aria-hidden="true"
-        />
-        Loading...
-      </Button>
-      
+            <Spinner
+              as="span"
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            Loading...
+          </Button>      
         </Container>
       );
     case "informTimeout":
       return <InformTimeout />;
     case "seeOutcome":
-      return <SeeOutcome nftViewAddress={args[0]} address={args[1]} />;
+      return <SeeOutcome getId ={args[0]} nftViewAddress={args[1]} address={args[2]} />;
     case "showBid":
-      return <ShowBid nftViewAddress={args[0]} getBid={args[1]} address={args[2]}/>; 
+      return <ShowBid getId ={args[0]}  nftViewAddress={args[1]} getBid={args[2]} address={args[3]}/>; 
     case "isAuctionOn":
       return (isAuctionOnReady ? 
-      (        <IsAuctionOn nftViewAddress={args[0]} isAuctionOn={isAuctionOn} />
+      (        <IsAuctionOn getId ={args[0]}  nftViewAddress={args[1]} isAuctionOn={isAuctionOn} />
       ) : (
         <Container style={{
           display: "grid",
@@ -53,16 +52,15 @@ const BidderViews = ({appState, args, getBidReady, getBid, isAuctionOnReady, isA
       }} className="mt-4">
           <h1>Waiting the Contract...</h1>
           <Button variant="info" disabled>
-        <Spinner
-          as="span"
-          animation="grow"
-          size="sm"
-          role="status"
-          aria-hidden="true"
-        />
-        Loading...
-      </Button>
-      
+            <Spinner
+              as="span"
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            Loading...
+          </Button>      
         </Container>
       ));
     default:
@@ -78,27 +76,25 @@ const BidderViews = ({appState, args, getBidReady, getBid, isAuctionOnReady, isA
   }} className="mt-4">
       <h1>Waiting the Contract...</h1>
       <Button variant="info" disabled>
-    <Spinner
-      as="span"
-      animation="grow"
-      size="sm"
-      role="status"
-      aria-hidden="true"
-    />
-    Loading...
-  </Button>
-  
+        <Spinner
+          as="span"
+          animation="grow"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+        Loading...
+      </Button>  
     </Container>
   );
 };
 
 export default BidderViews;
 
-export const GetBid = ({nftViewAddress, getBid}) => {
-
+export const GetBid = ({getId, nftViewAddress, getBid}) => {
     const [bid, setBid] = useState(0);
-    const handleSet = () => {
-        const bid2= parseCurrency(bid)
+    const handleSet = () => {      
+        const bid2= Reach.parseCurrency(bid)
         getBid(bid2)
     } 
   return (
@@ -110,21 +106,21 @@ export const GetBid = ({nftViewAddress, getBid}) => {
         padding: "2rem",
         marginTop:"2rem"
     }}>
-      <div style={{textAlign:"center"}}>
-            <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} />                                
-            <hr></hr>                               
-            </div>
-    <Form.Group>
-      <Form.Label><h3> </h3><h3>Please, Enter Your Bid</h3></Form.Label>
-      <Form.Control
-        
-        value={bid}
-        onChange={(e) => setBid(e.target.value)}
-        type="number"
-        placeholder=" Enter Bid"
-      />
-    </Form.Group>
-    <Button variant="info" onClick={handleSet} >Give Offer</Button>
-    </Container>
+        <div style={{textAlign:"center"}}>
+          <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} />
+          <h2>{`ID: ${getId}`}</h2>
+          <hr></hr>                               
+        </div>
+        <Form.Group>
+          <Form.Label><h3> </h3><h3>Please, Enter Your Bid</h3></Form.Label>
+          <Form.Control        
+            value={bid}
+            onChange={(e) => setBid(e.target.value)}
+            type="number"
+            placeholder=" Enter Bid"
+          />
+        </Form.Group>
+        <Button variant="info" onClick={handleSet} >Give Offer</Button>
+      </Container>
   );
 };
