a4976ed2e2a0c8831fd28d7a115ed9e3ccb57f63
aalimsahin <73793382+aalimsahin@users.noreply.github.com>
2021-06-09 00:56:55 +0300

    Update Bidder.js

--- src/components/participants/Bidder.js
+++ src/components/participants/Bidder.js
@@ -1,99 +1,79 @@
 import { Component } from "react";
 import * as Backend from "../../build/index.main.mjs";
-import * as Reach from "@reach-sh/stdlib/ALGO";
 import { Context } from "../../Context";
 import BidderViews from "./BidderViews";
+import {loadStdlib} from '@reach-sh/stdlib';
+const Reach = loadStdlib('ALGO');
 
 export class Bidder extends Component {
     static contextType = Context;
-
     constructor(props) {
         super(props);
-
         this.state = {
             appState: "",
             args: [],
             resGetBid: null,
-            resIsAuctionOn: null,
-            
+            resIsAuctionOn: null,            
         };
-
         this.getBidExt = this.getBidExt.bind(this);
         this.isAuctionOnExt = this.isAuctionOnExt.bind(this);
     }
 
     componentDidMount() {
         const [, , , , , , ctc, , , ,] = this.context;
-
         this.interval = setInterval(async () => this.updateBalance(), 5000);
-
         Backend.Bidder(ctc[0], this);
     }
 
     async updateBalance() {        
         const [account, , , setBalance] = this.context;
-
         const balance = Reach.formatCurrency(await Reach.balanceOf(account), 4);
         setBalance(balance);
     }
 
     async informTimeout() {
-        console.log("informTimeout");
-
-        this.setState({
+         this.setState({
             appState: "informTimeout",
         });
     }
 
-    async showBid(nftViewAddress, bid, address) {
-        console.log("showBid");
-
+    async showBid(getId, nftViewAddress, bid, address) {
          const addressFormat = Reach.formatAddress(address);
-         const bidFormat = Reach.formatCurrency(bid, 4);
-         
+         const bidFormat = Reach.formatCurrency(bid, 4);         
          this.setState({
              appState: "showBid",
-             args: [nftViewAddress, bidFormat ,addressFormat],
+             args: [getId ,nftViewAddress, bidFormat ,addressFormat],
          })
      }
 
-     async seeOutcome(nftViewAddress, address) {
-        console.log("seeOutcome");
-
+     async seeOutcome(getId, nftViewAddress, address) {
         const addressFormat = Reach.formatAddress(address);
-
         this.setState({
             appState: "seeOutcome",
-            args: [nftViewAddress, addressFormat],
+            args: [getId, nftViewAddress, addressFormat],
             
         })
     }
 
-    async getBid(nftViewAddress) {
+    async getBid(getId ,nftViewAddress) {
         const bid = await new Promise(res => {
-            console.log("getBid is called");
-
             this.setState({
                 appState: "getBid",
-                args: [nftViewAddress],
+                args: [getId, nftViewAddress],
                 resGetBid: res,
             });
         });
-        console.log(bid);
         return bid;
     }
     getBidExt(bid) {
-        console.log("getBidExt is called");
         this.state.resGetBid(bid);
     }
 
-    async isAuctionOn(nftViewAddress) {
-        console.log("isAuctionOn");
-
+    async isAuctionOn(getId, nftViewAddress) {
         const response = await new Promise (res => {
             this.setState({
                 appState: "isAuctionOn",
-                args: [nftViewAddress],
+                args: [getId,nftViewAddress],
                 resIsAuctionOn: res,
             })
         });
@@ -111,9 +91,7 @@ export class Bidder extends Component {
             getBidReady={this.state.resGetBid !== null}
             getBid={this.getBidExt} 
             isAuctionOnReady={this.state.resIsAuctionOn !== null}
-            isAuctionOn={this.isAuctionOnExt}
-            
-            />);
-            
+            isAuctionOn={this.isAuctionOnExt} />
+        );            
     }
 }
