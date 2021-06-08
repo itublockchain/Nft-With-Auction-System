f0b37cff6a5ed19e0345b5426d1e83a1189e178b
aalimsahin <73793382+aalimsahin@users.noreply.github.com>
2021-06-09 00:58:27 +0300

    Update PlayerViews.js

--- src/components/participants/PlayerViews.js
+++ src/components/participants/PlayerViews.js
@@ -1,13 +1,10 @@
 import React from "react";
-
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
-
 export const InformTimeout = () => {
-
     return (        
         <Container style={{
             display: "grid",
@@ -20,61 +17,57 @@ export const InformTimeout = () => {
         </Container>
     );
 }
-export const SeeOutcome = ({nftViewAddress, address }) => { 
+export const SeeOutcome = ({getId, nftViewAddress, address }) => { 
 
     const adres = {address}.address.toString()
     const addresssum = adres.slice(0,10) + "  . . . . .  "  + adres.slice((adres.length-10), adres.length);
-    // kimin kazandığını ekle
-        return (
-            <Container style={{
-                display: "grid",
-                backgroundColor: "white",
-                boxShadow: "rgba(0, 0, 0, 0.2) 0px 5px 20px",
-                borderRadius: "0.3rem",
-                padding: "2rem",
-            }} className="mt-4">
-                <div style={{textAlign:"center"}}>
-            <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} />                                
-            <hr></hr>                               
+    return (
+        <Container style={{
+            display: "grid",
+            backgroundColor: "white",
+            boxShadow: "rgba(0, 0, 0, 0.2) 0px 5px 20px",
+            borderRadius: "0.3rem",
+            padding: "2rem",
+        }} className="mt-4">
+            <div style={{textAlign:"center"}}>
+                <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} />
+                <h2>{`ID: ${getId}`}</h2>                              
+                <hr></hr>                               
             </div>
-                <h2>{`Winner Address: ${addresssum}`}</h2> 
-            </Container>
-        );
-    }
+            <h2>{`Winner Address: ${addresssum}`}</h2> 
+        </Container>
+    );
+}
 
-
-
-export const ShowBid = ({nftViewAddress, getBid, address }) => {
+export const ShowBid = ({getId, nftViewAddress, getBid, address }) => {
     const adres = {address}.address.toString()
     const addresssum = adres.slice(0,10) + "  . . . . .  "  + adres.slice((adres.length-10), adres.length);
-    console.log(getBid);
-
 
     return (
-      <Container style={{
-        display: "grid",
-        backgroundColor: "white",
-        boxShadow: "rgba(0, 0, 0, 0.2) 0px 5px 20px",
-        borderRadius: "0.3rem",
-        padding: "2rem",
-        marginTop:"2rem"
-    }}>
-        <div style={{textAlign:"center"}}>
-            <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} />                                
-            <hr></hr>                               
+        <Container style={{
+            display: "grid",
+            backgroundColor: "white",
+            boxShadow: "rgba(0, 0, 0, 0.2) 0px 5px 20px",
+            borderRadius: "0.3rem",
+            padding: "2rem",
+            marginTop:"2rem"
+        }}>
+            <div style={{textAlign:"center"}}>
+                <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} />
+                <h2>{`ID: ${getId}`}</h2>                              
+                <hr></hr>                               
             </div>
-        <h1>
-          Bid: <small>{getBid}</small> Algo
-        </h1>
-        <h1>
-          Bidder Address: <small>{addresssum}</small>
-        </h1>
-      </Container>
+            <h1>
+                Bid: <small>{getBid}</small> Algo
+            </h1>
+            <h1>
+                Bidder Address: <small>{addresssum}</small>
+            </h1>
+        </Container>
     );
-  };
+};
 
-export const IsAuctionOn = ({nftViewAddress, isAuctionOn}) => {
-
+export const IsAuctionOn = ({getId, nftViewAddress, isAuctionOn}) => {
     const handleReject = () => {
         isAuctionOn(false);
     }
@@ -91,9 +84,9 @@ export const IsAuctionOn = ({nftViewAddress, isAuctionOn}) => {
             padding: "2rem",
         }} className="mt-4">
             <div style={{textAlign:"center"}}>
-            <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} /> 
-                                           
-            <hr></hr>                               
+                <img alt= "NFT" src={nftViewAddress} style={{borderRadius: "0.3rem", border: "3px solid #000", width: '300px'}} />
+                <h2>{`ID: ${getId}`}</h2>
+                <hr></hr>                               
             </div>
             <h2 className="text-center">You are the owner of NFT.</h2>
             <p className="text-center">Do you want to start auction?</p>
